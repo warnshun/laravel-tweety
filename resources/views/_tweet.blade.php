998ed68668e6974a446c4c8b171b52d700837a6b
yang <him121415@gmail.com>
2021-03-07 22:44:50 +0800

    Homepage design

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,18 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文
+        </p>
+    </div>
+</div>
